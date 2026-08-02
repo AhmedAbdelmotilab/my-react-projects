3927cadd25136a06ef921f4fe57d6c46aa8dc01a
Ahmed Abdelmotilab <a.abdelmotilab@gmail.com>
2026-08-02 15:28:54 +0300

    Add new components and state management for quiz application
    - Introduced AppReducer and DateCounterReducer for managing application state.
    - Created Error, Header, Loader, Main components for UI structure.
    - Implemented Zustand state management in AppZustand and DateCounterZustand components.
    - Added a new store for quiz questions.
    - Included logo image for branding in the application.

---
+++ React Apps/07-react-quiz/src/components/Reducer/AppReducer.tsx
@@ -0,0 +1,12 @@
+import Main from "../Zustand/Main";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+export default App;
--- React Apps/07-react-quiz/src/main.tsx
+++ React Apps/07-react-quiz/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppReducer from "./components/Reducer/AppReducer";
+import AppZustand from "./components/Zustand/AppZustand";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AppZustand />
+    <AppReducer />
   </StrictMode>,
-)
+);
